c012bded8a9b751ae94ef4bfc21c8ceaacf188ec
Mahaboobsab Makandar | Sahil <sahilmakandar2492@gmail.com>
2024-05-14 12:02:32 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,19 +1,17 @@
 from typing import Any, Text, Dict, List
 
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
 class ActionFallback(Action):
 
-     def name(self) -> Text:
-         return "action_default_fallback"
+    def name(self) -> Text:
+        return "action_default_fallback"
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(tracker.latest_message["text"][1:])
+        dispatcher.utter_message(tracker.latest_message["text"][1:-1])
 
         return []
