9b7aee18cea4cd094cebdf9395d1c194f8002b73
Mahaboobsab Makandar | Sahil <mahaboobsab.goa@gmail.com>
2024-05-14 11:41:28 +0530

    Initial commit

---
+++ actions/actions.py
@@ -0,0 +1,19 @@
+from typing import Any, Text, Dict, List
+
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionFallback(Action):
+
+     def name(self) -> Text:
+         return "action_default_fallback"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(tracker.latest_message["text"][1:])
+
+        return []
